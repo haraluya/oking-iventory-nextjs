0b64f4f79d0135d9ae51926d06255b673a93d5e2
haraluya <haraluya777@gmail.com>
2025-08-18 14:30:24 +0800

    next.config修正

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const nextConfig = {
-  // 明確指定輸出模式為 'standalone'。
-  // 這會產生一個標準的、包含伺服器依賴的輸出目錄，
-  // 讓 Firebase 的框架感知部署功能可以 100% 準確地識別它。
-  output: 'standalone',
-};
-
-module.exports = nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 明確指定輸出模式為 'standalone'。
+  // 這會產生一個標準的、包含伺服器依賴的輸出目錄，
+  // 讓 Firebase 的框架感知部署功能可以 100% 準確地識別它。
+  output: 'standalone',
+};
 
 export default nextConfig;
